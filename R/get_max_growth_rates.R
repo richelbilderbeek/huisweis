a701733094ec3492bd6e948764b1e65229d54272
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-18 10:09:54 +0200

    Fix doc

--- R/get_max_growth_rates.R
+++ R/get_max_growth_rates.R
@@ -1,4 +1,5 @@
 #' Get the maximum growth rates of a number of species
+#' @inheritParams default_params_doc
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_max_growth_rates <- function(n_species) {
--- R/get_mortality_rates.R
+++ R/get_mortality_rates.R
@@ -1,6 +1,7 @@
 #' Get the mortality rates for any number of species
+#' @inheritParams default_params_doc
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_mortality_rates <- function(n_species) {
- rep(0.25, n_species)
+  rep(0.25, n_species)
 }
