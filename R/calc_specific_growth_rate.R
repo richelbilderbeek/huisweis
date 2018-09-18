4e0bbfc016a277842f08a7a4eaa15addbdbf1aa4
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-18 06:58:47 +0200

    Fix doc

--- R/calc_specific_growth_rate.R
+++ R/calc_specific_growth_rate.R
@@ -1,5 +1,20 @@
 #' Calculate the specific growth rates, equation 3
+#' @param resource_densities resource densities
+#' @param max_growth_rate max growth rate of the focal species
+#' @param half_saturation_resources half-saturation constants for
+#'   this focal species for each of the resources
+#' @return the growth rate of the focal species
 #' @author Richel J.C. Bilderbeek
+#' @examples
+#'   max_growth_rate <- 12.34
+#'   resource_densities <- 23.45
+#'   half_saturation_resources <- 34.56
+#'   specific_growth_rate <- calc_specific_growth_rate(
+#'     resource_densities = resource_densities,
+#'     max_growth_rate = max_growth_rate,
+#'     half_saturation_resources = half_saturation_resources
+#'   )
+#'   expect_equal(4.988329598, specific_growth_rate)
 #' @export
 calc_specific_growth_rate <- function(
   resource_densities,
